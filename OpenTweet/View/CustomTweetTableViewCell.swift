2b1b78325203e995c565cebcefed0472d7dc0d83
Danilo Silveira <danilo.nobrega88@gmail.com>
2024-07-24 14:25:54 -0400

    [Bonus] Highlight mentions @usernames

--- OpenTweet/View/CustomTweetTableViewCell.swift
+++ OpenTweet/View/CustomTweetTableViewCell.swift
@@ -22,6 +22,7 @@ class CustomTweetTableViewCell: UITableViewCell {
     }
     
     override func prepareForReuse() {
+        // stop the download in case the image go off screen
         self.avatarImageView.sd_cancelCurrentImageLoad()
     }
 
@@ -29,8 +30,15 @@ class CustomTweetTableViewCell: UITableViewCell {
         usernameLabel.text = tweet.author
         dateLabel.text = tweet.date.toString()
         tweetContentTextview.isUserInteractionEnabled = true
+        //loading using SDWebImage to cache the image after the download
         avatarImageView.sd_setImage(with: URL(string: tweet.avatar ?? ""), placeholderImage: UIImage(systemName: "person"))
-        tweetContentTextview.text = tweet.content
+        //Highlight the mentions (@username)
+        let tweetContent = (tweet.content as NSString)
+        let usernameHighlight = tweetContent.range(of: "@\\w.*?\\b", options: .regularExpression, range: NSMakeRange(0, tweetContent.length))
+        let attTweet = NSMutableAttributedString(string: tweet.content, attributes: [.font: UIFont.systemFont(ofSize: 16)])
+            attTweet.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: 17), range: usernameHighlight)
+            tweetContentTextview.attributedText = attTweet
+        
     }
     
 }
